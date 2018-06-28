8f128a1ca1cf2ea12624589c78d75a7417c8c829
Jorge Diaz <jorge7soccer@gmail.com>
2018-06-28 10:35:08 -0400

    ENH: Changes made to conform to PEP8.

--- suitcase/pizza_box/handlers.py
+++ suitcase/pizza_box/handlers.py
@@ -1,16 +1,22 @@
 import pandas as pd
 import os
 
-# These transformations are the transformations necessary 
+# These transformations are the transformations necessary
 # to convert the hex values that come from the ADC's into units of Volts.
 fc = 7.62939453125e-05
-adc2counts = lambda x: ((int(x, 16) >> 8) - 0x40000) * fc \
-        if (int(x, 16) >> 8) > 0x1FFFF else (int(x, 16) >> 8)*fc
-enc2counts = lambda x: int(x) if int(x) <= 0 else -(int(x) ^ 0xffffff - 1)
+
+
+def adc2counts(x):
+    return ((int(x, 16) >> 8) - 0x40000) * fc \
+            if (int(x, 16) >> 8) > 0x1FFFF else (int(x, 16) >> 8)*fc
+
+
+def enc2counts(x):
+    return int(x) if int(x) <= 0 else -(int(x) ^ 0xffffff - 1)
 
 
 class PizzaBoxANHandler():
-    
+
     def __init__(self, resource_path, chunk_size=1024):
         '''
         adds the chunks of data to a list for specific file
@@ -24,51 +30,45 @@ class PizzaBoxANHandler():
         '''
         self._name = resource_path
         self.chunks_of_data = []
-        chunk = [data for data in pd.read_csv(resource_path, 
-            chunksize=chunk_size, delimiter = " ", header = None) ]
-        
+        chunk = [data for data in pd.read_csv(resource_path,
+                 chunksize=chunk_size,
+                 delimiter=" ",
+                 header=None)]
         num_cols = len(chunk[0].columns)
-
         columns = ['time (s)', 'time (ns)', 'index']
         columns_leftover = num_cols - len(columns)
         columns = columns + [f'adc {i}' for i in range(columns_leftover)]
-        
+
         for chunk in chunk:
             chunk.columns = columns
-            
             for column in range(columns_leftover):
-                chunk.iloc[:, column + 3] = chunk.iloc[:, column + 3].apply(adc2counts)
-            
+                chunk.iloc[:, column + 3] = \
+                            chunk.iloc[:, column + 3].apply(adc2counts)
             chunk['timestamp'] = chunk['time (s)'] + 1e-9*chunk['time (ns)']
-            column_keys = ['timestamp'] + [f'adc {i}' for i in range(columns_leftover)]
+            column_keys = ['timestamp'] + \
+                          [f'adc {i}' for i in range(columns_leftover)]
             chunk = chunk[column_keys]
-            self.chunks_of_data.append(chunk)    
-        
+            self.chunks_of_data.append(chunk)
 
     def __call__(self, chunk_num, column):
         '''
-        Returns 
+        Returns
         -------
         result: dataframe object
             specified chunk number/index from list of all chunks created
         '''
-        cols = {
-                'timestamp': self.chunks_of_data[chunk_num]['timestamp'],
-                'counts': self.chunks_of_data[chunk_num][f'adc {column}']
-               }
-        return pd.DataFrame(cols, columns = ['timestamp', 'counts'])
-         
-        
+        cols = {'timestamp': self.chunks_of_data[chunk_num]['timestamp'],
+                'counts': self.chunks_of_data[chunk_num][f'adc {column}']}
+        return pd.DataFrame(cols, columns=['timestamp', 'counts'])
 
     def get_file_size(self, datum_kwarg_gen):
         filename = f'{self._name}'
         size = os.path.getsize(filename)
-               
         return size
 
 
 class PizzaBoxENHandler():
-    
+
     def __init__(self, resource_path, chunk_size=1024):
         '''
         adds the chunks of data to a list for specific file
@@ -82,19 +82,20 @@ class PizzaBoxENHandler():
         '''
         self._name = resource_path
         self.chunks_of_data = []
-        for chunk in pd.read_csv(resource_path, chunksize=chunk_size, 
-                names = ['time (s)', 'time (ns)', 'encoder', 'index', 'di'], 
-                delimiter = " ", header=None):
+        column_names = ['time (s)', 'time (ns)', 'encoder', 'index', 'di']
+        for chunk in pd.read_csv(resource_path,
+                                 chunksize=chunk_size,
+                                 names=column_names,
+                                 delimiter=" ",
+                                 header=None):
             chunk['timestamp'] = chunk['time (s)'] + 1e-9*chunk['time (ns)']
             chunk['encoder'] = chunk['encoder'].apply(enc2counts)
-            chunk = chunk.drop(columns = ['time (s)', 'time (ns)', 'index', 'di'])
             chunk = chunk[['timestamp', 'encoder']]
             self.chunks_of_data.append(chunk)
 
-
     def __call__(self, chunk_num, column):
         '''
-        Returns 
+        Returns
         -------
         result: dataframe object
             specified chunk number/index from list of all chunks created
@@ -102,9 +103,7 @@ class PizzaBoxENHandler():
         result = self.chunks_of_data[chunk_num]
         return result
 
-
     def get_file_size(self, datum_kwarg_gen):
         filename = f'{self._name}'
         size = os.path.getsize(filename)
-               
         return size
